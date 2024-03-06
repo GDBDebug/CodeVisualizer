818b911e6037ec3653c4295a4625066d4f261c90
thebenzo <amanvrma5799@gmail.com>
2024-03-06 14:54:42 +0530

    Change react app to show date-time and setup proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [currentDateTime, setCurrentDateTime] = useState(0);
+
+  useEffect(() => {
+    fetch("/now")
+      .then((result) => result.json())
+      .then((data) => {
+        setCurrentDateTime(data.now);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Current date and time is {currentDateTime}.</p>
       </header>
     </div>
   );
