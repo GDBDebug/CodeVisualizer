dce4ef7df8963621b50346d90d4de1160399759f
thebenzo <amanvrma5799@gmail.com>
2024-03-17 14:27:30 +0530

    Create menubar component

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -2,15 +2,19 @@ import { Box, Flex } from "@chakra-ui/react";
 import CodeEditor from "./CodeEditor";
 import OutputArea from "./OutputArea";
 import UserInputArea from "./UserInputArea";
+import Menubar from "./Menubar";
 
 const Landing = () => {
   return (
     <Box>
-      <Flex>
-        <CodeEditor />
-        <Flex flexDirection="column">
-          <UserInputArea />
-          <OutputArea />
+      <Flex flexDirection="column">
+        <Menubar />
+        <Flex>
+          <CodeEditor />
+          <Flex flexDirection="column">
+            <UserInputArea />
+            <OutputArea />
+          </Flex>
         </Flex>
       </Flex>
     </Box>
---
+++ frontend/src/components/Menubar.js
@@ -0,0 +1,52 @@
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+  Tooltip,
+} from "@chakra-ui/react";
+import { DownloadIcon, AddIcon } from "@chakra-ui/icons";
+
+const Menubar = () => {
+  return (
+    <Box
+      m={4}
+      px={4}
+      py={2}
+      border="4px"
+      borderColor="#008080"
+      borderRadius="20px"
+      overflow="hidden"
+    >
+      <Flex aminWidth="max-content" alignItems="center" gap="6">
+        <Button borderRadius="10px" bg="#008080">
+          Compile and Run
+        </Button>
+        <Button borderRadius="10px" bg="#008080">
+          Visualize
+        </Button>
+        <Spacer />
+        <ButtonGroup isAttached>
+          <Tooltip hasArrow label="Save to file">
+            <IconButton icon={<DownloadIcon />}></IconButton>
+          </Tooltip>
+          <IconButton icon={<AddIcon />}></IconButton>
+        </ButtonGroup>
+        <Menu>
+          <MenuButton as={Button}>Options</MenuButton>
+          <MenuList>
+            <MenuItem>Work in Progress</MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Menubar;
